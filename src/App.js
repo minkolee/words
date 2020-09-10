242e390a22b01f7038aa70fe2e4abf8d07ee1b3d
minkolee <minkolee@gmail.com>
2020-09-10 16:01:25 +0800

    做个背单词的APP, 不知道能做成什么样, 数据先搞好了

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import words from "./data/words";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            words: words
+        }
+    }
+
+    render() {
+
+        return (
+            <div className="App">
+                <table className='word'>
+                    <thead>
+                    <tr>
+                        <th>WORD</th>
+                        <th>MEANING</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    {this.state.words.map(
+                        word => (
+                            <tr key={word.name}>
+                                <td><span className='name'>{word.name}</span></td>
+                                <td><span className='meaning'>{word.meaning}</span></td>
+                            </tr>
+                        )
+                    )
+
+                    }
+
+
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
+
 
 export default App;
